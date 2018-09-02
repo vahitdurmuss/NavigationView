401d93e8976f40bba849da65eebecbf72ffc735b
vahitdurmus <vahitdurmuss@gmail.com>
2018-09-02 19:34:52 +0300

    01 NW

---
+++ app/src/main/java/com/example/user/navigationviewexample/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.user.navigationviewexample;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+    private NavigationView mNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDrawerLayout=(DrawerLayout)findViewById(R.id.drawer_layout);
+        mNavigationView=(NavigationView)findViewById(R.id.nav_view);
+
+        mNavigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                menuItem.setChecked(true);
+                mDrawerLayout.closeDrawers();
+                return  true;
+            }
+        });
+    }
+}
